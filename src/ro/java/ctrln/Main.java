78cc34bb83529c92e3b279e60d317c32a32fd6f9
florin <git@github.com:florinplaiasu/PrimaAplicatieJava.git>
2022-07-10 21:50:39 +0300

    Adaugare comentarii

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -4,6 +4,6 @@ public class Main {
     //JAR - Java Archive
     //JAVA_HOME
     public static void main(String[] args) {
-	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+	    System.out.println(/*args este parametrul metodei*/"Invat limbajul Java si despre masina virtuala Java!");
     }
 }
